89d62f6ca92bef6aae528a27803e2f046788c9d0
smaghili <97967312+smaghili@users.noreply.github.com>
2024-07-30 15:35:01 +0330

    Update main.py

--- main.py
+++ main.py
@@ -2,29 +2,15 @@ import asyncio
 import signal
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
-from config import load_config, save_config
+from config import API_TOKEN, ADMIN_CHAT_ID
 from handlers import register_handlers
-
-config = load_config()
-API_TOKEN = config.get("API_TOKEN")
-ADMIN_CHAT_ID = config.get("ADMIN_CHAT_ID")
-backup_interval_minutes = config.get("backup_interval_minutes")
-backup_task = None
 
 bot = None
 dp = None
 
 async def initialize_bot():
-    global API_TOKEN, ADMIN_CHAT_ID, bot, dp, config, backup_interval_minutes, backup_task
+    global bot, dp
     try:
-        if not API_TOKEN:
-            API_TOKEN = input("Please enter your bot token: ").strip()
-            config["API_TOKEN"] = API_TOKEN
-            save_config(config)
-        if not ADMIN_CHAT_ID:
-            ADMIN_CHAT_ID = input("Please enter the admin chat ID: ").strip()
-            config["ADMIN_CHAT_ID"] = ADMIN_CHAT_ID
-            save_config(config)
         bot = Bot(token=API_TOKEN)
         storage = MemoryStorage()
         dp = Dispatcher(storage=storage)
