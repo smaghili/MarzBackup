81669b80aec3c0b7496b79dbac885c7ccf75c096
smaghili <97967312+smaghili@users.noreply.github.com>
2024-07-31 11:07:01 +0330

    fix backup interval

--- main.py
+++ main.py
@@ -16,12 +16,20 @@ storage = MemoryStorage()
 dp = Dispatcher(storage=storage)
 
 async def scheduled_backup():
+    last_backup_time = 0
     while True:
         config = load_config()
         interval = config.get("backup_interval_minutes", 0)
+        current_time = asyncio.get_event_loop().time()
+        
         if interval > 0:
-            await asyncio.sleep(interval * 60)
-            await create_and_send_backup(bot)
+            time_since_last_backup = current_time - last_backup_time
+            if time_since_last_backup >= interval * 60:
+                await create_and_send_backup(bot)
+                last_backup_time = current_time
+            else:
+                wait_time = (interval * 60) - time_since_last_backup
+                await asyncio.sleep(min(wait_time, 60))
         else:
             await asyncio.sleep(60)  # Check every minute if interval is not set
 
