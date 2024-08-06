79e856819fec1494ecb1761890ffa6722872cea0
smaghili <97967312+smaghili@users.noreply.github.com>
2024-08-06 23:26:04 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 import asyncio
 import logging
 import sys
-from datetime import datetime, timedelta
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
-from aiogram.filters.command import Command
-from config import load_config, save_config, DB_NAME, DB_CONTAINER, DB_PASSWORD, DB_TYPE
+from config import load_config
 from handlers import register_handlers
-from backup import create_and_send_backup
 
 # Configure logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -25,40 +22,6 @@ if not API_TOKEN or not ADMIN_CHAT_ID:
 bot = Bot(token=API_TOKEN)
 storage = MemoryStorage()
 dp = Dispatcher(storage=storage)
-
-async def schedule_backups():
-    last_backup_time = datetime.now()
-    while True:
-        try:
-            config = load_config()
-            backup_interval = config.get('backup_interval_minutes', 60)  # Default to 60 minutes if not set
-            interval_change_time = config.get('interval_change_time', 0)
-            
-            current_time = datetime.now()
-            time_since_last_backup = (current_time - last_backup_time).total_seconds() / 60
-            
-            logging.info(f"Current time: {current_time}")
-            logging.info(f"Last backup time: {last_backup_time}")
-            logging.info(f"Time since last backup: {time_since_last_backup:.2f} minutes")
-            logging.info(f"Backup interval: {backup_interval} minutes")
-            
-            if time_since_last_backup >= backup_interval:
-                logging.info("Initiating backup...")
-                success = await create_and_send_backup(bot)
-                if success:
-                    last_backup_time = current_time
-                    logging.info("Backup completed successfully.")
-                else:
-                    logging.error("Backup failed.")
-            
-            # Calculate the time to wait until the next backup
-            wait_time = max(0, (backup_interval - time_since_last_backup) * 60)
-            logging.info(f"Waiting for {wait_time:.2f} seconds until next backup check.")
-            
-            await asyncio.sleep(wait_time)
-        except Exception as e:
-            logging.error(f"Error in backup scheduler: {e}")
-            await asyncio.sleep(60)  # Wait for 1 minute before retrying
 
 async def validate_config():
     config = load_config()
@@ -107,7 +70,6 @@ async def validate_config():
 async def on_startup(bot: Bot):
     await validate_config()
     await bot.send_message(chat_id=ADMIN_CHAT_ID, text="MarzBackup bot has been successfully started!")
-    asyncio.create_task(schedule_backups())
 
 async def main():
     # Register all handlers
