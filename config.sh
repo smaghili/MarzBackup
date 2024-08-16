500406953de179c998cbdc66ffaa95c088f0b0d0
smaghili <97967312+smaghili@users.noreply.github.com>
2024-08-16 17:51:16 +0330

    Delete config.sh

--- config.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# User-specific configurations
-INSTALL_DIR="/opt/MarzBackup"
-CONFIG_DIR="/opt/marzbackup"
-LOG_FILE="/var/log/marzbackup.log"
-PID_FILE="/var/run/marzbackup.pid"
-
-# Add any other custom configurations here
